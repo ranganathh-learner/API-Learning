a39b3e1cebdfff5e2cc84d942a04f3cf780b62f6
M K <Ranganatha.MK@unisys.com>
2019-12-25 12:17:36 +0530

    Changed

--- db/clouddb.js
+++ db/clouddb.js
@@ -3,11 +3,12 @@
  */
 
  // mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
+//mongodb+srv://ranganathh:<password>@cluster0-bbb5r.azure.mongodb.net/test?retryWrites=true&w=majority
 
-const DB_USER = "admin"
-const DB_PASSWORD = "admin123"
-const DB_NAME = "acmetravel"
-const CLUSTER_HOST = "cluster0-46e5h.mongodb.net"
+const DB_USER = "ranganathh"
+const DB_PASSWORD = "1Welcome123"
+const DB_NAME = "acmetravels"
+const CLUSTER_HOST = "cluster0-bbb5r.azure.mongodb.net"
 
 // Setup the DB URI
 exports.DB_URI= "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+CLUSTER_HOST+"/"+DB_NAME+"?retryWrites=true&w=majority"
